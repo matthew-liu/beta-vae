b88d93dc334a10d6e24b35dbc84581845f2c6f66
Matthew Liu <liux44@cs.washington.edu>
2018-12-05 16:54:10 -0800

    defined model

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 import utils
 
 # CelebA (VAE)
@@ -12,19 +11,17 @@ import utils
 # Latents 32
 # Decoder Deconv reverse of encoder. ReLU activation. Gaussian.
 
-# Test set: Average loss: 2.4985, Accuracy: 4061/10000 (41%)
-
 Base_Accuracy = 0
-d_out = 200  # number of classes
 
-
-# input size: 64, 64, 3
 
 class BetaVAE(nn.Module):
-    def __init__(self):
+
+    def __init__(self, latent_size=32, beta=250):
         super(BetaVAE, self).__init__()
 
         self.best_accuracy = Base_Accuracy
+        self.latent_size = latent_size
+        self.beta = beta
 
         # encoder
         self.encoder = nn.Sequential(
@@ -33,16 +30,41 @@ class BetaVAE(nn.Module):
             self._conv(32, 64),
             self._conv(64, 64),
         )
+        self.fc_mu = nn.Linear(256, latent_size)
+        self.fc_var = nn.Linear(256, latent_size)
+
+        # decoder
+        self.decoder = nn.Sequential(
+            self._deconv(64, 64),
+            self._deconv(64, 32),
+            self._deconv(32, 32, 1),
+            self._deconv(32, 3),
+            nn.Sigmoid()
+        )
+        self.fc_z = nn.Linear(latent_size, 256)
+
+    def encode(self, x):
+        x = self.encoder(x)
+        x = x.view(-1, 256)
+        return self.fc_mu(x), self.fc_var(x)
+
+    def sample(self, mu, logvar):
+        std = torch.exp(0.5*logvar)  # e^(1/2 * log(std^2))
+        eps = torch.randn_like(std)  # random ~ N(0, 1)
+        return eps.mul(std).add_(mu)
+
+    def decode(self, z):
+        z = self.fc_z(z)
+        z = z.view(-1, 64, 2, 2)
+        return self.decoder(z)
 
     def forward(self, x):
-        x = self.encoder(x)
-        print(x.size())
-        exit()
-        return x
+        mu, logvar = self.encode(x)
+        z = self.sample(mu, logvar)
+        rx = self.decode(z)
+        return rx, mu, logvar
 
-    # layers
-    @staticmethod
-    def _conv(in_channels, out_channels):
+    def _conv(self, in_channels, out_channels):
         return nn.Sequential(
             nn.Conv2d(
                 in_channels, out_channels,
@@ -52,20 +74,21 @@ class BetaVAE(nn.Module):
             nn.ReLU()
         )
 
-    def _deconv(self, channel_num, kernel_num):
+    # out_padding is used to ensure output size matches EXACTLY of conv2d;
+    # it does not actually add zero-padding to output :)
+    def _deconv(self, in_channels, out_channels, out_padding=0):
         return nn.Sequential(
             nn.ConvTranspose2d(
-                channel_num, kernel_num,
-                kernel_size=4, stride=2, padding=1,
+                in_channels, out_channels,
+                kernel_size=4, stride=2, output_padding=out_padding
             ),
-            nn.BatchNorm2d(kernel_num),
-            nn.ReLU(),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
         )
 
-    @staticmethod
-    def loss(recon_x, x, mu, logvar, beta):
-        # Reconstruction + KL divergence losses summed over all elements and batch
-        recon_loss = F.binary_cross_entropy(recon_x, x, reduction='sum')
+    def loss(self, recon_x, x, mu, logvar):
+        # reconstruction losses are summed over all elements and batch, then divided by batch size
+        recon_loss = F.binary_cross_entropy(recon_x, x, reduction='sum') / x.shape[0]
 
         # see Appendix B from VAE paper:
         # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
@@ -73,7 +96,7 @@ class BetaVAE(nn.Module):
         # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
         kl_diverge = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-        return recon_loss + beta * kl_diverge
+        return recon_loss + self.beta * kl_diverge
 
     def save_model(self, file_path, num_to_keep=1):
         utils.save(self, file_path, num_to_keep)
--- train.py
+++ train.py
@@ -3,9 +3,10 @@ import torch
 import torch.nn as nn
 from torchvision import datasets
 from torchvision import transforms
-from torchvision.transforms.functional import crop
+import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt
+import multiprocessing
 import time
 import preprocess as prep
 import models
@@ -35,15 +36,41 @@ def train(model, device, train_loader, optimizer, epoch, log_interval):
     return train_loss
 
 
-train_ids, test_ids = prep.split_dataset()
-print('#train_images:', len(train_ids))
-print('#test_images:', len(test_ids))
+# parameters
+BATCH_SIZE = 256
+TEST_BATCH_SIZE = 10
+EPOCHS = 50
 
-# attr_map, id_attr_map = prep.get_attributes()
-# smile_ims = prep.get_attr(attr_map, id_attr_map, 'smiling')
+LEARNING_RATE = 1e-4
+WEIGHT_DECAY = 1e-5
+
+USE_CUDA = True
+PRINT_INTERVAL = 100
+
+# training code
+train_ids, test_ids = prep.split_dataset()
+print('num train_images:', len(train_ids))
+print('num test_images:', len(test_ids))
 
 data_train = prep.ImageLoader(train_ids)
 data_test = prep.ImageLoader(test_ids)
 
+use_cuda = USE_CUDA and torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+print('Using device', device)
+print('num cpus:', multiprocessing.cpu_count())
+
+kwargs = {'num_workers': multiprocessing.cpu_count(),
+          'pin_memory': True} if use_cuda else {}
+
+train_loader = torch.utils.data.DataLoader(data_train, batch_size=BATCH_SIZE, shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(data_test, batch_size=TEST_BATCH_SIZE, shuffle=False, **kwargs)
+
+# attr_map, id_attr_map = prep.get_attributes()
+# smile_ims = prep.get_attr(attr_map, id_attr_map, 'smiling')
+
 model = models.BetaVAE()
-print(model)
+# print(model)
+
+for batch_idx, data in enumerate(train_loader):
+    output = model(data)
